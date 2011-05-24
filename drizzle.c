e36035ccf12051ae7eec75f17662dd53a3db1c7e
Tejas Dinkar <tejas@gja.in>
2011-05-25 02:27:13 +0530

    Moved all the public methods to it's own helpers file

--- drizzle.c
+++ drizzle.c
@@ -10,35 +10,6 @@ void init_drizzle_status();
 void init_drizzle_connection();
 void init_drizzle_result();
 
-static VALUE do_not_use_this_constructor(VALUE self)
-{
-  rb_raise(rb_eNameError, "private method `new' called for %s:Class.", rb_class2name(CLASS_OF(self)));
-}
-
-VALUE drizzle_gem_to_string_array(char **array, long count)
-{
-  VALUE ary = rb_ary_new2(count);
-
-  long i;
-  for(i = 0; i < count; i++)
-    rb_ary_push(ary, rb_str_new2(array[i]));
-
-  return ary;
-}
-
-VALUE drizzle_gem_create_class_with_private_constructor(const char *name, VALUE super)
-{
-  VALUE ret = rb_define_class_under(DrizzleModule, name, super);
-  rb_define_method(ret, "initialize", do_not_use_this_constructor, 0);
-  return ret;
-}
-
-void drizzle_gem_assert_value_is_ok(drizzle_return_t ret)
-{
-  if(ret != DRIZZLE_RETURN_OK)
-    rb_raise(rb_eIOError, "Action Failed with error code: %d", ret);
-}
-
 void Init_drizzle()
 {
   init_drizzle_module();
