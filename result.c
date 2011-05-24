1f91449e5cfb1386e316deacaa5e0f3a67a3077e
Tejas Dinkar <tejas@gja.in>
2011-05-25 01:38:10 +0530

    Added a row_count method

--- result.c
+++ result.c
@@ -2,7 +2,23 @@
 
 #define SELF_TYPE drizzle_result_st
 
+static VALUE buffer(VALUE self)
+{
+  read_self_ptr();
+
+  CHECK_OK(drizzle_result_buffer(self_ptr));
+}
+
+static VALUE row_count(VALUE self)
+{
+  read_self_ptr();
+  buffer(self);
+  return UINT2NUM(drizzle_result_row_count(self_ptr));
+}
+
 void init_drizzle_result()
 {
   DrizzleResult = drizzle_gem_create_class_with_private_constructor("Result", rb_cObject);
+  rb_define_method(DrizzleResult, "row_count", row_count, 0);
+  rb_define_method(DrizzleResult, "buffer", buffer, 0);
 }
