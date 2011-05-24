4e150658533f1050af549b894d43a4466cc2cddd
Tejas Dinkar <tejas@gja.in>
2011-05-25 01:20:55 +0530

    Added a verbose set

--- status.c
+++ status.c
@@ -28,10 +28,19 @@ static VALUE error(VALUE self)
   return rb_str_new2(drizzle_error(self_ptr));
 }
 
+static VALUE set_verbose(VALUE self, VALUE newVerbocity)
+{
+  read_self_ptr();
+
+  int verbocity = NUM2INT(newVerbocity);
+  drizzle_set_verbose(self_ptr, verbocity);
+}
+
 void init_drizzle_status()
 {
   DrizzleStatus = drizzle_gem_create_class_with_private_constructor("Status", rb_cObject);
   rb_define_method(DrizzleStatus, "clone", clone, 0);
   rb_define_method(DrizzleStatus, "add_tcp_connection", add_tcp_connection, 0);
   rb_define_method(DrizzleStatus, "error", error, 0);
+  rb_define_method(DrizzleStatus, "verbose=", set_verbose, 1);
 }
