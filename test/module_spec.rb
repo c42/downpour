27ef15f09db75bdc29b6b8851b4dbd204470551c
Tejas Dinkar <tejas@gja.in>
2011-05-25 23:30:24 +0530

    Renamed the spec files

--- test/module_spec.rb
+++ test/module_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require './spec_helper'
 
 describe Drizzle do
   it "should have the version number set" do
--- test/smoke_spec.rb
+++ test/smoke_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require './spec_helper'
 
 describe Drizzle do
   before(:each) do
