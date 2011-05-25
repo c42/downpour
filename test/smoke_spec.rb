e49cdaae6af35bc5fa1b17feb3ddf126e02bbaa4
Tejas Dinkar <tejas@gja.in>
2011-05-25 23:56:58 +0530

    Added more smoke tests

--- test/smoke_spec.rb
+++ test/smoke_spec.rb
@@ -1,13 +1,38 @@
 require './spec_helper'
 
 describe Drizzle do
+
   before(:each) do
     @status = Drizzle.create
-    @conn = @status.add_tcp_connection
+    @conn = create_connection(@status)
   end
 
-  it "should be able to count records" do
-    results = @conn.query "select * from Test1"
-    results.row_count.should == 3
+  context "a select query" do
+    before(:each) do
+      @results = @conn.query "select * from Test1"
+    end
+
+    it "should be able to count records" do
+      @results.row_count.should == 3
+    end
+
+    it "should be able to count columns" do
+      @results.column_count.should == 1
+    end
+
+    it "should be able to get a row" do
+      @results.next_row.should == ["foo"]
+      #@results.next_row.should == ["bar"]
+      #@results.next_row.should == ["baz"]
+      #@results.next_row.should == []
+    end
   end
+
+  after(:each) do
+    error = @status.error
+    if(error != "")
+      puts error
+    end
+  end
+
 end
