13b66a04804bf707621720bb1322ef16b2f870fb
Tejas Dinkar <tejas@gja.in>
2011-05-27 10:10:28 +0530

    Revert "Just forcing drizzle linking"
    This reverts commit 1007253f31fe77be22022b2546e4105bc0540449.

--- ext/downpour/extconf.rb
+++ ext/downpour/extconf.rb
@@ -3,7 +3,7 @@ require 'mkmf'
 extension_name = 'downpour'
 dir_config(extension_name)
 
-$LDFLAGS += ' -ldrizzle'
+have_library("drizzle", "drizzle_create")
 $CFLAGS += " -I/usr/local/include/libdrizzle-1.0 -I/usr/include/libdrizzle-1.0"
 
 create_makefile(extension_name)
