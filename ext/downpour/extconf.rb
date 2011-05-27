6a408db87450f77aec5ef28bc84cbf97c42584ff
Tejas Dinkar <tejas@gja.in>
2011-05-27 10:00:58 +0530

    Some debian stuff

--- ext/downpour/extconf.rb
+++ ext/downpour/extconf.rb
@@ -3,7 +3,7 @@ require 'mkmf'
 extension_name = 'downpour'
 dir_config(extension_name)
 
-have_library("drizzle")
+have_library("drizzle", "drizzle_create")
 $CFLAGS += " -I/usr/local/include/libdrizzle-1.0 -I/usr/include/libdrizzle-1.0"
 
 create_makefile(extension_name)
--- start_local_drizzle_server_for_tests.sh
+++ start_local_drizzle_server_for_tests.sh
@@ -7,5 +7,5 @@ mkdir -p tmp/database
 drizzled --no-defaults --datadir=$PWD/tmp/database -d
 
 echo Creating a new test database
-echo "create database test;" | drizzle
-drizzle test < spec/bootstrap.sql
+echo "create database test;" | drizzle -u $USER
+drizzle -u $USER test < spec/bootstrap.sql
