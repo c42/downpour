48b044b54850f5111764394504607767dbaf273b
Tejas Dinkar <tejas@gja.in>
2011-05-31 01:18:04 +0530

    Bumped up the version and added pkgconfig

--- ext/downpour/extconf.rb
+++ ext/downpour/extconf.rb
@@ -1,9 +1,15 @@
 require 'mkmf'
 
+libdrizzle = 'libdrizzle-1.0'
+
 extension_name = 'downpour'
 dir_config(extension_name)
 
-have_library("drizzle", "drizzle_create")
-$CFLAGS += " -I/usr/local/include/libdrizzle-1.0 -I/usr/include/libdrizzle-1.0"
+if(!system("pkg-config #{libdrizzle}"))
+  raise "Cannot find pkg-config or #{libdrizzle}"
+end
+
+$LDFLAGS += " " + `pkg-config --libs #{libdrizzle}`
+$CFLAGS += " " + `pkg-config --cflags #{libdrizzle}`
 
 create_makefile(extension_name)
