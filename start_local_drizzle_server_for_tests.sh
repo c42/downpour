1546be9e0f28e730ca69d52fe5816541f56d7647
Tejas Dinkar <tejas@gja.in>
2011-06-02 23:23:36 +0530

    Adding the socket in the right place :-)

--- start_local_drizzle_server_for_tests.sh
+++ start_local_drizzle_server_for_tests.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/usr/sbin:/usr/local/sbin
 DATABASE=$PWD/tmp/database
 rm -rf $DATABASE
 mkdir -p $DATABASE
-drizzled --no-defaults --datadir=$DATABASE -d
+drizzled --no-defaults --datadir=$DATABASE -d --mysql-unix-socket-protocol.path "$DATABASE/mysql.socket"
 
 echo Creating a new test database
 echo "create database test;" | drizzle -u $USER
