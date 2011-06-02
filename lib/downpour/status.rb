4149bdd769570b42eaf3d13d4a87d7c366f0d03c
Tejas Dinkar <tejas@gja.in>
2011-06-03 02:48:31 +0530

    Added some nice memory management stuff

--- lib/downpour/status.rb
+++ lib/downpour/status.rb
@@ -1,15 +1,25 @@
 module Downpour
   class Status
     def add_query(connection, query)
-      _add_query(connection, query)
+      new_query = _add_query(connection, query)
+      pending_queries << new_query
+      new_query
     end
 
     def run!
-      _run!
+      pending_queries.delete(_run!)
     end
 
     def run_all!
       _run_all!
+      pending_queries.clear
+    end
+
+    def pending_queries
+      if(@pending_queries == nil)
+        @pending_queries = []
+      end
+      @pending_queries
     end
   end
 end
--- spec/downpour/concurrent_spec.rb
+++ spec/downpour/concurrent_spec.rb
@@ -8,24 +8,58 @@ describe "a concurrent query" do
     @query2 = @status.add_query(@conn2, "select name from Test1 where name like '%ar'");
   end
 
-  it "should buffer all queries" do
-    @status.run_all!
-    @query1.result.should be_buffered
-    @query2.result.should be_buffered
+  context "when running all" do
+    it "should buffer all queries" do
+      @status.run_all!
+      @query1.result.should be_buffered
+      @query2.result.should be_buffered
+    end
+
+    it "should run all queries" do
+      @status.run_all!
+      @query1.result.next_row.should == ["foo"]
+      @query2.result.next_row.should == ["bar"]
+    end
+
+    it "should clear all pending queries" do
+      @status.pending_queries.size.should == 2
+      @status.run_all!
+      @status.pending_queries.should be_empty
+    end
   end
 
-  it "should run all queries" do
-    @status.run_all!
-    @query1.result.next_row.should == ["foo"]
-    @query2.result.next_row.should == ["bar"]
-  end
+  context "when executing one at a time" do
+    shared_examples_for "a working concurrent query" do
+      it "should run a query" do
+        rows = []
+        rows << @status.run!.result.next_row
+        rows << @status.run!.result.next_row
+        rows.should include(["foo"])
+        rows.should include(["bar"])
+      end
+    end
 
-  it "should run a query" do
-    rows = []
-    rows << @status.run!.result.next_row
-    rows << @status.run!.result.next_row
-    rows.should include(["foo"])
-    rows.should include(["bar"])
-    @status.run!.should be_nil
+    it_should_behave_like "a working concurrent query"
+
+    it "should return nil after all queries are executed" do
+      @status.run!
+      @status.run!
+      @status.run!.should be_nil
+    end
+
+    it "should release pending queries" do
+      query = @status.run!
+      @status.pending_queries.should_not include(query)
+      @status.pending_queries.size.should == 1
+    end
+
+    context "when the caller has gced the query" do
+      before(:each) do
+        @query1 = @query2 = nil
+        GC.start
+      end
+
+      it_should_behave_like "a working concurrent query"
+    end
   end
 end
