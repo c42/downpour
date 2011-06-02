9b2426a1ae96df265836bf9d10077f9a98ecd116
Tejas Dinkar <tejas@gja.in>
2011-06-02 21:48:54 +0530

    Looks like my memory fixing made all the concurrent specs pass

--- spec/downpour/concurrent_spec.rb
+++ spec/downpour/concurrent_spec.rb
@@ -3,20 +3,20 @@ describe "a concurrent query" do
   before(:each) do
     @status = Downpour.create
     @conn = create_connection(@status)
-    #@conn2 = create_connection(@status)
+    @conn2 = create_connection(@status)
     @query1 = @status.add_query(@conn, "select * from Test1 where name like '%oo'");
-    #@query2 = @status.add_query(@conn2, "select name from Test1 where name like '%ar'");
+    @query2 = @status.add_query(@conn2, "select name from Test1 where name like '%ar'");
   end
 
   it "should buffer all queries" do
     @status.run_all!
     @query1.result.should be_buffered
-    #@query2.result.should be_buffered
+    @query2.result.should be_buffered
   end
 
   it "should run all queries" do
     @status.run_all!
     @query1.result.next_row.should == ["foo"]
-    #@query2.result.next_row.should == ["bar"]
+    @query2.result.next_row.should == ["bar"]
   end
 end
